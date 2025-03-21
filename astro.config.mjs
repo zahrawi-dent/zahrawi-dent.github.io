a7034c76fd66f86aff1f31398a28da3fba5a36ba
MuhammadSwa <muhammadatefsw@protonmail.com>
2025-03-21 19:27:06 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import pagefind from "astro-pagefind";
+import { remarkReadingTime } from "./src/remark-reading-time.mjs";
 
 import preact from "@astrojs/preact";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://zhahrawi-dent.github.io",
-  integrations: [preact()],
+  integrations: [preact(), pagefind(), icon()],
+
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 
   vite: {
     plugins: [tailwindcss()],
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
+import calculateReadingTime from "reading-time";
+import { fromMarkdown } from "mdast-util-from-markdown";
+import { toString } from "mdast-util-to-string";
+
 function formatDate(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
@@ -7,5 +11,28 @@ function formatDate(date: Date): string {
   };
   return new Date(date).toLocaleDateString(undefined, options);
 }
+function getReadingTime(s: string) {
+  const wpm = 200;
 
-export { formatDate };
+  const n = s
+    .replace(/[^\w\s]/gi, "")
+    .replaceAll("\r", "")
+    .replaceAll("\n", "")
+    .split(" ").length;
+
+  return Math.ceil(n / wpm);
+}
+
+// const getReadingTime = (text: string): string | undefined => {
+//   if (!text || !text.length) return undefined;
+//   try {
+//     const { minutes } = calculateReadingTime(toString(fromMarkdown(text)));
+//     if (minutes && minutes > 0) {
+//       return `${Math.ceil(minutes)} min read`;
+//     }
+//     return undefined;
+//   } catch (e) {
+//     return undefined;
+//   }
+// };
+export { formatDate, getReadingTime };
