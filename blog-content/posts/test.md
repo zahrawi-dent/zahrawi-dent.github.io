0da34653b7484fcaaefa9cddd9fcc5d8cdcdd3ee
MuhammadSwa <muhammadatefsw@protonmail.com>
2025-04-29 20:45:33 +0300

    edit blog

---
+++ blog-content/posts/test.md
@@ -0,0 +1,12 @@
+---
+title: "Test Post"
+pubDate: 2022-07-01
+lastUpdated: 2022-08-08
+description: "This is the first post of my new Astro blog."
+author: "Astro Learner"
+image:
+  url: "https://docs.astro.build/assets/rose.webp"
+  alt: "The Astro logo on a dark background with a pink glow."
+tags: [ "blogging", "learning in public"]
+---
+this is a proposed change 
--- src/content.config.ts
+++ src/content.config.ts
@@ -30,5 +30,24 @@ const blog = defineCollection({
   }),
 });
 
+const posts = defineCollection({
+  loader: glob({
+    pattern: "**/[^_]*.{md,mdx}",
+    base: "blog-content/posts"
+  }),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    lastUpdated: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+  }),
+})
+
 // Export a single `collections` object to register your collection(s)
-export const collections = { blog };
+export const collections = { blog, posts };
